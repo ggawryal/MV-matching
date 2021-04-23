16b77a1a66979f3cb428d44383616aa7b58c9559
ggawryal <ggawryal@gmail.com>
2021-04-23 20:37:49 +0200

    changed matched to mate vector

--- matching.cpp
+++ matching.cpp
@@ -32,15 +32,15 @@ struct DSU {
 
 enum EdgeType {NotScanned, Prop, Bridge};
 struct Edge {
-    int from,to;
+    int to;
     int other;
-    bool matched;
     EdgeType type = NotScanned;
 };
 
 const int INF = 1e9;
 
 int n;
+vector<int> mate;
 vector<vector<Edge> > graph;
 vector<vector<int> > predecessors;
 vector<int> ddfsPredecessorsPtr;
@@ -60,10 +60,10 @@ vector<pair<pii,pii> > myBridge; // bridge, bud[bridge]
 
 int minlvl(int u) { return min(evenlvl[u], oddlvl[u]); }
 
-int tenacity(Edge e) {
-    if(e.matched)
-        return oddlvl[e.from] + oddlvl[e.to] + 1;
-    return evenlvl[e.from] + evenlvl[e.to] + 1;
+int tenacity(pii edge) {
+    if(mate[edge.st] == edge.nd)
+        return oddlvl[edge.st] + oddlvl[edge.nd] + 1;
+    return evenlvl[edge.st] + evenlvl[edge.nd] + 1;
 }
 
 /*
@@ -106,10 +106,8 @@ int ddfsMove(vector<int>& stack1, const int color1, vector<int>& stack2, const i
 
 
 //returns {r0, g0} or {bottleneck, bottleneck}
-pair<int,int > ddfs(Edge e, vector<int>& out_support) {
-    assert(e.type == Bridge);
-    
-    vector<int> Sr = {bud[e.from]}, Sg = {bud[e.to]};
+pair<int,int > ddfs(pii e, vector<int>& out_support) {
+    vector<int> Sr = {bud[e.st]}, Sg = {bud[e.nd]};
     if(Sr[0] == Sg[0])
         return {Sr[0],Sg[0]};
 
@@ -132,29 +130,16 @@ pair<int,int > ddfs(Edge e, vector<int>& out_support) {
     }
 }
 
-bool isVertexMatched(int u) {
-    for(auto e:graph[u])
-        if(e.matched)
-            return true;
-    return false;
-}
-
 queue<int> removedVerticesQueue;
 
-void removeAndPushToQueue(int u) {if(!removed[u]) {removed[u] = 1; removedVerticesQueue.push(u);}}
+void removeAndPushToQueue(int u) {removed[u] = 1; removedVerticesQueue.push(u);}
 
 void flip(int u, int v) {
+    if(removed[u] || removed[v] || mate[u] == v) return;//flipping only unmatched edges
     removeAndPushToQueue(u);
     removeAndPushToQueue(v);
-    bool found = false;
-    for(auto &a:graph[v]) {
-        if(a.to == u) {
-            a.matched ^= 1;
-            graph[a.to][a.other].matched ^= 1;
-            found = true;
-        }
-    }
-    assert(found);
+    mate[u] = v;
+    mate[v] = u;
 }
 
 void augumentPath(int a, int b, bool initial=false);
@@ -178,8 +163,7 @@ void augumentPath(int u, int v, bool initial) {
     if(u == v) return;
     if(!initial && minlvl(u) == evenlvl[u]) { //simply follow predecessors
         assert(predecessors[u].size() == 1); //u should be evenlevel (last minlevel edge is matched, so there is only one predecessor)
-        int x = predecessors[u][0];
-        flip(u,x);
+        int x = predecessors[u][0]; //no need to flip edge since we know it's matched
 
         int idx = 0;
         while(bud[predecessors[x][idx]] != bud[x]) {
@@ -210,20 +194,13 @@ void augumentPath(int u, int v, bool initial) {
 }
 
 void checkGraph() {
-    for(auto a:graph) {
-        int cnt = 0;
-        for(auto e:a) {
-            assert(e.matched == graph[e.to][e.other].matched);
-            if(e.matched)
-                cnt++;
-        }
-        assert(cnt <= 1);
-    }
+    for(int i=0;i<n;i++)
+        assert(mate[i] == -1 || mate[mate[i]] == i);
 }
 
 bool bfs() {
     vector<vector<int> > verticesAtLevel(n);
-    vector<vector<Edge> > bridges(2*n+2);
+    vector<vector<pii> > bridges(2*n+2);
     vector<int> removedPredecessorsSize(n);
 
     auto setLvl = [&](int u, int lev) {
@@ -232,14 +209,14 @@ bool bfs() {
     };
 
     for(int u=0;u<n;u++)
-        if(!isVertexMatched(u))
+        if(mate[u] == -1)
             setLvl(u,0);
 
     bool foundPath = false;  
     for(int i=0;i<n && !foundPath;i++) {
         for(auto u : verticesAtLevel[i]) {
             for(auto& e:graph[u]) {
-                if(e.type == NotScanned && (oddlvl[u] == i) == e.matched) {
+                if(e.type == NotScanned && (oddlvl[u] == i) == (mate[u] == e.to)) {
                     if(minlvl(e.to) >= i+1) {
                         e.type = Prop;
                         graph[e.to][e.other].type = Prop;
@@ -251,31 +228,34 @@ bool bfs() {
                     else {
                         e.type = Bridge;
                         graph[e.to][e.other].type = Bridge;
-                        if(tenacity(e) < INF) {
-                            bridges[tenacity(e)].push_back(e);
+                        if(tenacity({u,e.to}) < INF) {
+                            bridges[tenacity({u,e.to})].push_back({u,e.to});
                         }
                     }
                 }
             }
         }
         
-
         for(auto b : bridges[2*i+1]) {
-            if(removed[bud[b.from]] || removed[bud[b.to]])
+            if(removed[bud[b.st]] || removed[bud[b.nd]])
                 continue;
             vector<int> support;
-            pair<int,int> ddfsResult = ddfs(b,support);
-            pair<pii,pii> curBridge = {{b.from,b.to},{bud[b.from], bud[b.to]}};
+            auto ddfsResult = ddfs(b,support);
+            pair<pii,pii> curBridge = {b,{bud[b.st], bud[b.nd]}};
+            /*even when we found two disjoint paths, we create fake petal, with bud in the end of second path
+            the support of this bridge will be these two pathes and some other vertices, which have bases on this paths, so we will remove them and this will not affect corectness
+            using this trick, we can simply call augumentPath on these two ends - the first end is just above fake bud, so it will augument exactly the path we need
+            the only problem is that some vertices in this support will be uncorrectly classified as inner/outer, so we need to pass initial=true flag to fix this case*/
             for(auto v:support) {
                 if(v == ddfsResult.second) continue; //skip bud
                 myBridge[v] = curBridge;
                 bud.linkTo(v,ddfsResult.second);
 
-                //this part of code is only needed when bottleneck found, but it doesn't mess up anything when called on two paths
+                //this part of code is only needed when bottleneck found, but it doesn't mess up anything when called on two paths 
                 setLvl(v,2*i+1-minlvl(v));
                 for(auto f : graph[v])
-                    if(evenlvl[v] > oddlvl[v] && f.type == Bridge && tenacity(f) < INF && !f.matched)
-                        bridges[tenacity(f)].push_back(f);
+                    if(evenlvl[v] > oddlvl[v] && f.type == Bridge && tenacity({v,f.to}) < INF && mate[v] != f.to)
+                        bridges[tenacity({v,f.to})].push_back({v,f.to});
             }
 
             if(ddfsResult.first != ddfsResult.second) {
@@ -285,7 +265,7 @@ bool bfs() {
                     int v = removedVerticesQueue.front();
                     removedVerticesQueue.pop();
                     for(auto e : graph[v])
-                        if(e.type == Prop && minlvl(e.to) > minlvl(e.from) && !removed[e.to] && ++removedPredecessorsSize[e.to] == predecessors[e.to].size())
+                        if(e.type == Prop && minlvl(e.to) > minlvl(v) && !removed[e.to] && ++removedPredecessorsSize[e.to] == predecessors[e.to].size())
                             removeAndPushToQueue(e.to);
                 }
             }
@@ -295,6 +275,7 @@ bool bfs() {
 }
 
 void mvMatching() {
+    mate = vector<int>(n,-1);
     do {
         for(auto&a: graph)
             for(auto&e:a)
@@ -320,14 +301,11 @@ int32_t main(){
         int a,b,isMatched;
         cin >> a >> b >> isMatched;
         isMatched = false;
-        graph[a].push_back({a, b, (int)graph[b].size(), isMatched, NotScanned});
-        graph[b].push_back({b, a, (int)graph[a].size()-1, isMatched, NotScanned});
+        graph[a].push_back({b, (int)graph[b].size(), NotScanned});
+        graph[b].push_back({a, (int)graph[a].size()-1, NotScanned});
     }
    
     mvMatching();
-    int cnt = 0;
-    for(int i=0;i<n;i++)
-        if(isVertexMatched(i))
-            cnt++;
+    int cnt =  (n-count(mate.begin(),mate.end(),-1));
     cout<<cnt/2<<endl;//<<" after "<<iters<<" iterations"<<endl;
 }
